40753c35246b5d6f2d9e79f1f8ddac364f4443cf
root <root@oscar-HP-1000-Notebook-PC>
2016-09-08 14:12:00 -0600

    no funciona

--- Guia5_POO2_Oscar_Garcia/src/main/java/com/sv/udb/controlador/AlumnosBean.java
+++ Guia5_POO2_Oscar_Garcia/src/main/java/com/sv/udb/controlador/AlumnosBean.java
@@ -3,43 +3,71 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.sv.udb.controlador;
 
-import com.sv.udb.modelo.Alumno;
+import com.sv.udb.modelo.Alumnos;
 import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 /**
  *
- * @author Laboratorio
+ * @author root
  */
-@ManagedBean
-@RequestScoped
+@Named(value = "alumnosBean")
+@Dependent
 public class AlumnosBean {
-    private Alumno objeAlum;
+
+    private Alumnos objeAlum;
     private boolean guard;
 
     /**
-     * Creates a new instance of AlumnosBean
+     * Creates a new instance of AlumnoBean
      */
-    public AlumnosBean() {        
-        
+    public AlumnosBean() {
     }
-    
+
     @PostConstruct //Despues de iniciar la vista
-    public void init(){
-        this.objeAlum = new Alumno();
+    public void init() {
+        this.objeAlum = new Alumnos();
         this.guard = true;
     }
 
-    public Alumno getObjeAlum() {
+    public Alumnos getObjeAlum() {
         return objeAlum;
     }
 
-    public void setObjeAlum(Alumno objeAlum) {
+    public void setObjeAlum(Alumnos objeAlum) {
         this.objeAlum = objeAlum;
     }
-    
+
+    public boolean isGuard() {
+        return guard;
+    }
+
+    public void setGuard(boolean guard) {
+        this.guard = guard;
+    }
+
+    public void guard() {
+        boolean resp = false;
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            em.persist(this.objeAlum);
+            tx.commit();
+            resp = true;
+        } catch (Exception ex) {
+            tx.rollback();
+        }
+        em.close();
+        emf.close();
+    }
+
 }
